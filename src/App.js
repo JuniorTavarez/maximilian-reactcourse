4e32130144e27be2782af766129241383c782d3c
JuniorTavarez <Jtavarez8@gmail.com>
2021-01-10 10:59:59 -0500

    finish section 36 from course

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really Working!</p>
-        <Person />
-        <Person />
-        <Person />
-        <Person />
-        <Person />
+        <Person name="Junior" age="32" />
+        <Person name="Rebeca" age="26" />
+        <Person name="Jeremiah" age="4" />
+        <Person name="Zara" age="3">
+          My Hobbies: Dancing
+        </Person>
+        <Person name="Gideon" age="1" />
       </div>
     );
   }
